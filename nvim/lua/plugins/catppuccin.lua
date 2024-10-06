639794c17ef1d4aa3131a76677e7ebfcbf67a9e8
Francisco Fernando Martins Moras <francima@c1r10s8.42porto.com>
2024-10-06 18:02:26 +0100

    2

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,19 +1,11 @@
 return {
 	"catppuccin/nvim",
 	name = "catppuccin",
-	priority = 1000, 
+	priority = 1000,
 	config = function()
-		 require("catppuccin").setup({
-                transparent_background = true,  -- Enable transparency
-                term_colors = true,  -- Use terminal colors
-                styles = {
-                    comments = { "italic" },
-                    functions = { "bold" },
-                    keywords = { "bold" },
-                    strings = { "italic" },
-                    variables = { "italic" },
-                },
-            })
+		require("catppuccin").setup({
+			transparent_background = true,
+		})
 		vim.cmd.colorscheme "catppuccin"
 	end
 }
