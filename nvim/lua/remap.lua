cad4ef829e063f97b688a4edc9f1b38160b79877
Zico-wsl <francis.ffmm@proton.me>
2024-10-14 21:44:32 +0100

    commit lazygit1

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -2,4 +2,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>tm', ':vsplit term://%:p:h//bash<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-y>', '"+y', { noremap = true, silent = true })
+
 
