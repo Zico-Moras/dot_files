cfe66efbba670e1ac7e35d93b40a6f93ad5aa900
Zico-wsl <francis.ffmm@proton.me>
2024-10-18 18:17:17 +0100

    vamos

--- nvim/.config/nvim/lua/remap.lua
+++ nvim/.config/nvim/lua/remap.lua
@@ -2,6 +2,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>tm', ':vsplit term://%:p:h//bash<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<C-y>', '"+y', { noremap = true, silent = true })
-
-
+--vim.api.nvim_set_keymap('v', '<C-y>', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>yy', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>dd', '"+d', { noremap = true, silent = true })
